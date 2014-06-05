87db3fa87111003523fce55e83a3daa475a1365f
John Hu <huziyong@gmail.com>
2014-06-05 23:57:16 +0800

    New rails app with bootstrappers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupme::Application.config.secret_key_base = '280f1f370da743dfb430932af24beba3812fcefddea0de4acdf5c67bda3dcbc37277ab48db9d9234933dfa4852d12b85aa79c514e6059ef92391f01bc961ae24'
